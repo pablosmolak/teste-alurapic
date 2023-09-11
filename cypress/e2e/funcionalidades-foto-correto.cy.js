88fe10d0246dde02d371b9628b032ad4b99eeba6
Pablo Smolak <pablosmolak2004@hotmail.com>
2023-09-11 12:19:55 -0400

    criando teste para apagar foto

--- cypress/e2e/funcionalidades-foto-correto.cy.js
+++ cypress/e2e/funcionalidades-foto-correto.cy.js
@@ -15,6 +15,15 @@ describe('Funcionalidades Foto, cenário de sucesso', () => {
       cy.contains('Upload complete').should('be.visible')
       cy.get('.fa-home').click()
       cy.get('.img-thumbnail').should('have.length',1)
+      cy.wait(1000)
+      cy.get('.img-thumbnail').first().click()
+      cy.get('.fa-heart-o').click()
+      cy.wait(1000)
+      cy.get('[formcontrolname="comment"]').type('Lapada atrás de lapada ein')
+      cy.get('[type="submit"]').click()
+      cy.wait(5000)
+      cy.get('.fa-trash-o').click()
+
       
     })
   })
